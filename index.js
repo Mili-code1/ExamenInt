682ae2f679470062bfdb020c4b02ef580d8d4cec
Mili-code1 <miriamchalanuk@gmail.com>
2025-03-25 19:44:28 -0300

    Examen Integrar

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+function calcularMultiplicacion(a, b, c) {
+    let resultado = multiplicar(a, b, c);
+
+    console.log("Resultado de la multiplicación:", resultado);
+
+    imprimir(reultado);
+}
+
+const multiplicar = (x, y) => { x * y };
+
+calcularMultiplicacion(3, 7);
+// Resultado de la multiplicación: 21
